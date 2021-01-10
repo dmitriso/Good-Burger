fd5645af1ae3bbaf21cb510230bad4a6b77faf73
Dmitri So <dmitriso@yahoo.com>
2021-01-10 12:23:01 -0500

    Created schema.sql file of MySQL schema statement.

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(30) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+)
+
+SELECT * FROM burgers;
