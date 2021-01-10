a08297357b11219f50de5af1fb2cf6fcad3c66f6
Dmitri So <dmitriso@yahoo.com>
2021-01-10 12:57:30 -0500

    Setting up burgers_controller.js dependencies.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const good_burger = require('good_burger.js');
+
+nodule.exports = burgers_controller.js;
+
+// Create the router for the app, and export the router at the end of your file.
