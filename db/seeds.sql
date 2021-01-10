a14ad1974bdfc0573f7eb3bff7bf4fcd66371eb9
Dmitri So <dmitriso@yahoo.com>
2021-01-10 12:36:50 -0500

    Creted seeds.sql file of MySQL insert statements.

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers;
+
+-- SEED QUERIES
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Hamburger",false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Cheeseburger",false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Mushroomburger",false);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("Veggieburger",false);
