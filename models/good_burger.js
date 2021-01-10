bd40b274e0d8f97353efe5068b62baab5a70ae6d
Dmitri So <dmitriso@yahoo.com>
2021-01-10 12:56:36 -0500

    Setting up good_burger.js dependencies.

---
+++ models/good_burger.js
@@ -0,0 +1,6 @@
+const connection = require('connection.js');
+const orm = require('orm.js');
+
+module.exports = good_burger.js;
+
+// Create the code that will call the ORM functions using burger specific input for the ORM.
