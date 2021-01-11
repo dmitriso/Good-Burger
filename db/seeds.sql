1e1533fa5f3747df04b0fbbeeeb8557c7fccc5e5
Dmitri So <dmitriso@yahoo.com>
2021-01-11 16:53:12 -0500

    Refactoring seeds.sql file.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,20 @@
 USE burgers;
 
 -- SEED QUERIES
-INSERT INTO burgers (id, burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Goodburger",false);
+
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Hamburger",false);
 
-INSERT INTO burgers (id, burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Cheeseburger",false);
 
-INSERT INTO burgers (id, burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Mushroomburger",false);
 
-INSERT INTO burgers (id, burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES ("Veggieburger",false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sushiburger",true);
