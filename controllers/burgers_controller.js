6e7739abe9be429ff9830fbf3ed16a4c4f0b8211
Dmitri So <dmitriso@yahoo.com>
2021-01-12 10:00:57 -0500

    Added DELETE route  to remove a devoured burger by its id.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,26 +3,26 @@ const burger = require('../models/good_burger.js');
 const router = express.Router();
 
 // THIS DISPLAYS ALL THE BURGER DATA TO THE MAIN PAGE
-router.get("/", function (req, res) {
-    burger.all(function (data) {
+router.get("/", function(req, res) {
+    burger.all(function(data) {
         res.render("index", { burgers: data });
     });
 });
 
 // THIS ROUTE DISPLAYS THE BURGERS READY TO BE DEVOURED
-router.post("/api/burgers", function (req, res) {
+router.post("/api/burgers", function(req, res) {
     burger.create(["burger_name"], [req.body.burger_name],
-        function (result) {
+        function(result) {
             res.json({ id: result.insertId });
         });
 });
 
 // THIS ROUTE UPDATES THE BURGER DEVOURED STATUS
-router.put("/api/burgers/:id", function (req, res) {
+router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
     burger.update({
         devoured: req.body.devoured,
-    }, condition, function (result) {
+    }, condition, function(result) {
         if (result.chengedRows == 0) {
             return res.status(404).end();
         } else {
@@ -31,7 +31,15 @@ router.put("/api/burgers/:id", function (req, res) {
     });
 });
 
-
+// THIS ROUTE DELETES A DEVOURED BURGER
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function (result) {
+            if (result.chengedRows == 0) {
+                return res.status(404).end();
+            } res.status(200).end();
+        });
+});
 
 // EXPORTING ROUTER MODULE
 module.exports = router;
