2addd18ed274323d5fb2efad0c4d02223eca2926
Dmitri So <dmitriso@yahoo.com>
2021-01-11 22:40:42 -0500

    Created POST route for /api/burgers to create and render a new burger to index.handlebars.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,6 +9,14 @@ router.get("/", function (req, res) {
     });
 });
 
+// THIS ROUTE DISPLAYS THE BURGERS READY TO BE DEVOURED
+router.post("/api/burgers", function (req, res) {
+    burger.create(["burger_name"], [req.body.burger_name],
+        function (result) {
+            res.json({ id: result.insertId });
+        });
+});
+
 
 // EXPORTING ROUTER MODULE
 module.exports = router;
