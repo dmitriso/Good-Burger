2efa7dd9f2d369f8885453f57102148e09fc9e34
Dmitri So <dmitriso@yahoo.com>
2021-01-12 09:48:57 -0500

    Creating serrver.js file to run the application.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// MIDDLEWARE
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// SET HANDLEBARS
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// IMPORT ROUTES TO GIVE THE SERVER ACCESS TO THEM
+app.use(routes);
+
+// SERVER LISTEN
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
