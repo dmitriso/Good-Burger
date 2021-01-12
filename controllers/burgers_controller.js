b1e6b34fdf51c397e0cb036df045f89d20db1db3
Dmitri So <dmitriso@yahoo.com>
2021-01-11 22:42:46 -0500

    Created PUT route for /api/burgers/:id to update the devoured status of a burger in index.handlebars.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,6 +17,21 @@ router.post("/api/burgers", function (req, res) {
         });
 });
 
+// THIS ROUTE UPDATES THE BURGER DEVOURED STATUS
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.update({
+        devoured: req.body.devoured,
+    }, condition, function (result) {
+        if (result.chengedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
 
 // EXPORTING ROUTER MODULE
 module.exports = router;
